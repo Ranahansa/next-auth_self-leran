e55b1bd2388167350b0f3fdc0cc06fad311bb50a
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-06-04 19:04:36 +0530

    Remove unused components and pages

--- app/ClientMember/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-    return (
-        <div>Client Member</div>
-    )
-}
-
-export default page
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./(components)/NavBar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <div className="m-3">{children}</div>
+        {children}
       </body>
     </html>
   );
