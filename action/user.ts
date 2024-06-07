e001c1bedb441892813944bf581eab4dcca00672
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-06-07 13:39:30 +0530

    Update user registration to hash password before storing it

--- action/user.ts
+++ action/user.ts
@@ -3,6 +3,7 @@
 import connectDB from "@/lib/db";
 import { User } from "@/models/User";
 import { redirect } from "next/navigation";
+import { hash } from "bcryptjs";
 
 const register = async (formData: FormData) => {
     const firstName = formData.get("firstName") as string;
@@ -21,7 +22,9 @@ const register = async (formData: FormData) => {
         throw new Error("User already exists");
     }
 
-    await User.create({ firstName, lastName, email, password });
+    const hashedPassword = await hash(password, 10);
+
+    await User.create({ firstName, lastName, email, password: hashedPassword });
     console.log("User registered successfully");
     redirect("/login");
 };
